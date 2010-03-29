e76159edf9b8c673df7cb651c2a503009c5ce5bf
James Lecker <james.lecker@mwcradio.com>
2010-03-29 17:04:51 -0500

    template tags

--- askmeanything/templatetags/poll_tags.py
+++ askmeanything/templatetags/poll_tags.py
@@ -3,19 +3,19 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from django.contrib.contenttypes.models import ContentType
 
-from askmeanything.models import PublishedPoll
+from askmeanything.models import Poll, PublishedPoll
 
 register = template.Library()
 
 @register.tag
-def latest_poll_for(parser, token):
+def show_latest_poll_for(parser, token):
     try:
         (tag_name, publication_variable_name) = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError, "%r tag requires exactly one argument" % token.contents.split()[0]
-    return PollFormNode(publication_variable_name)
+    return PublishedPollFormNode(publication_variable_name)
 
-class PollFormNode(template.Node):
+class PublishedPollFormNode(template.Node):
     def __init__(self, publication_variable_name):
         self.publication_variable = template.Variable(publication_variable_name)
     
@@ -27,6 +27,25 @@ class PollFormNode(template.Node):
         publication_type = ContentType.objects.get_for_model(publication)
         try:
             poll = PublishedPoll.objects.filter(publication_type=publication_type, publication_id=publication.id).latest().poll
+        except ObjectDoesNotExist:
+            return ''
+        return '<script type="text/javascript" src="' + poll.get_absolute_url + '"></script>'
+
+@register.tag
+def show_poll(parser, token):
+    try:
+        (tag_name, poll_id) = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires exactly one argument" % token.contents.split()[0]
+    return PollFormNode(poll_id)
+
+class PollFormNode(template.Node):
+    def __init__(self, poll_id):
+        self.poll_id = poll_id
+    
+    def render(self, context):
+        try:
+            poll = Poll.objects.filter(id=self.poll_id)
         except ObjectDoesNotExist:
             return ''
         return '<script type="text/javascript" src="' + poll.get_absolute_url + '"></script>'
