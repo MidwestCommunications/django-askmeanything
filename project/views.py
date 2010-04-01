eacda78930ebce77e78268cccf32cc3908622e05
James Lecker <james.lecker@mwcradio.com>
2010-04-01 12:36:02 -0500

    updated README

--- project/views.py
+++ project/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render_to_response
 
 def test(request, poll_id):
-    return render_to_response('test.html', {'poll_id': poll_id})
+    return render_to_response('show.html', {'poll_id': poll_id})
